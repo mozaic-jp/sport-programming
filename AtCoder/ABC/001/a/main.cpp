2fe22aae19ce604df9f45c6008c0dea3243eba86
Sasajima Kensuke <sasajima77@gmail.com>
2019-09-25 00:47:17 +0900

    Create main.cpp

---
+++ AtCoder/ABC/001/a/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+//https://atcoder.jp/contests/abc001/submissions/5701454
+
+int main(){
+    int H1,H2,res;
+
+    cin >> H1 >> H2;
+    res = H1-H2;
+    cout << res << endl;
+}
