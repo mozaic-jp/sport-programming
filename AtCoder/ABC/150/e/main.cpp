c3686a74d7138952a66c19da933ea24b97081d7d
mozaic-jp <39161048+mozaic-jp@users.noreply.github.com>
2020-01-13 21:45:24 +0900

    あ

---
+++ AtCoder/ABC/150/e/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>       // for next_permutation
+#include <vector>
+#include <queue>
+#include <bits/stl_numeric.h>
+#include <math.h>
+
+#define DEBUG 0
+#define REP(i, n) for (long long i = 0; i < (n); i++) 
+using namespace std;
+
+typedef long long ll;
+typedef pair <int, int> pii;
+static const ll MOD = 1000000007;
+static const ll INF = 1000000000000000000LL;
+
+long myPow(long x, long n, long m){
+  if(n == 0)
+    return 1;
+  if(n % 2 == 0)
+    return myPow(x * x % m, n / 2, m);
+  else
+    return x * myPow(x, n - 1, m) % m;
+}
+
+int main() {
+
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    ll N;
+    cin >> N;
+
+    vector <ll> c(N);
+
+    REP(i, N)
+    {
+        cin >> c[i];
+    }
+    sort(c.rbegin(), c.rend());
+
+    ll tmp3 = myPow(2, N * 2 - 2, MOD);
+    ll tmp2 = tmp3 * 2 % MOD;
+    ll res = 0;
+
+    REP(i,N)
+    {
+        ll tmp;
+        tmp = tmp2 * c[i] % MOD;
+
+        res += tmp;
+        res %= MOD;
+
+        tmp2 += tmp3;
+        tmp2 %= MOD;
+    }
+
+    cout << res << endl;
+
+    return 0;
+}
