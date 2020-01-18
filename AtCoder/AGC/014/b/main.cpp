3ce8a61f9fbad4fa9f58a16bda0a40ec55215530
mozaic-jp <39161048+mozaic-jp@users.noreply.github.com>
2020-01-18 20:59:33 +0900

    a

---
+++ AtCoder/AGC/014/b/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstring>
+#include <math.h>
+#include <algorithm> // sort
+
+#define REP(i, n) for (long long i = 0; i < (n); i++) 
+typedef long long ll;
+static const ll INF = 1000000000000000000LL;
+using namespace std;
+
+const int MOD = 1000000007;
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    ll N, M;
+    cin >> N >> M;
+    ll res;
+    ll t;
+    string zero = "";
+    string a;
+    int l,r,m;
+    l = 0;
+    r = N;
+    m = (N-1) / 2;
+
+    return 0;    
+}
---
+++ AtCoder/OtherContests/keyence2020/a/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstring>
+#include <math.h>
+#include <algorithm> // sort
+
+#define REP(i, n) for (long long i = 0; i < (n); i++) 
+typedef long long ll;
+static const ll INF = 1000000000000000000LL;
+using namespace std;
+
+const int MOD = 1000000007;
+
+int main(){
+    ll N,res;
+    cin >> N;
+
+    cout << res << endl;
+
+    return 0;    
+}
